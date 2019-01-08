25d11f6bf84b6f1687f235597b83a31d36adc48d
Andrzej Senderecki <skidrzej@gmail.com>
2019-01-08 21:50:28 +0100

    Dodanie jQuery i animacji scroll do sekcji About Us i Work

--- scripts/app.js
+++ scripts/app.js
@@ -5,4 +5,27 @@ require('../scss/partials/_workSection.scss');
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    let $aboutLink = $('#about');
+    let $workLink = $('#work');
+    let $shop = $('#shop');
+    let $contact = $('#contact');
+
+    let $headerSection = $('.header-contaner');
+    let $aboutSection = $('.about-section-container');
+    let $workSection = $('.work-section-container');
+
+    $aboutLink.on('click', (event) => {
+        event.preventDefault();
+        $('html').animate({
+            scrollTop: $aboutSection.offset().top
+        }, 1000);
+    });
+
+    $workLink.on('click', (event) => {
+        event.preventDefault();
+        $('html').animate({
+            scrollTop: $workSection.offset().top
+        }, 1000);
+    });
+
 });
